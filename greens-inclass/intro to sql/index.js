6f2945f258bdef176e3256a54557980bce0932b2
Surya <surya.vallamkonda@gmail.com>
2023-11-16 10:12:19 -0500

    node sql

---
+++ greens-inclass/intro to sql/index.js
@@ -0,0 +1,65 @@
+// const sql3 = require("sqlite3").verbose();
+// const db = new sql3.Database("database.db");
+
+// function sqlPromise(query) {
+//   return new Promise((resolve, reject) => {
+//     db.all(query, (err, rows) => {
+//       if (err) reject(err);
+//       resolve(rows);
+//     });
+//   });
+// }
+
+// async function main() {
+//   let query = "SELECT c_name, c_strength FROM characters ORDER BY c_strength";
+//   let results = await sqlPromise(query);
+//   console.log(results);
+//   db.close();
+// }
+
+// main();
+
+const express = require("express");
+const app = express();
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("database.db");
+
+app.set("view engine", "ejs");
+
+function sqlPromise(query, params) {
+  return new Promise((resolve, reject) => {
+    db.all(query, params, (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
+    });
+  });
+}
+
+app.get("/", async (req, res) => {
+  let sqlquery = "SELECT * FROM characters";
+  let results = await sqlPromise(sqlquery, []);
+
+  let dictionary_out = {
+    results: results,
+  };
+  res.render("characters", dictionary_out);
+});
+
+app.get("/characters/:c_name", async (req, res) => {
+  const { c_name } = req.params;
+
+  let sqlquery = "SELECT * FROM characters WHERE c_name=(?)";
+  let sqlparams = [c_name];
+  let stats = await sqlPromise(sqlquery, sqlparams);
+
+  let dictionary_out = {
+    stats: stats,
+  };
+  console.log(dictionary_out);
+  res.render("hero", dictionary_out);
+});
+
+app.listen(8080, "0.0.0.0", () => {
+  console.log("server started");
+});
