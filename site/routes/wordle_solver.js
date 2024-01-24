91ae3fe047dd61bf214902a8c39a15240fee6492
Surya <surya.vallamkonda@gmail.com>
2024-01-23 23:56:14 -0500

    wordle

--- site/routes/wordle_solver.js
+++ site/routes/wordle_solver.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const cors = require("cors");
 const router = express.Router();
 router.use(express.json());
 
 const wordsFilePath = path.join(__dirname, "enable1.txt");
-const words = fs.readFileSync(wordsFilePath).toString().split("\n");
+let words = fs.readFileSync(wordsFilePath).toString().split("\n");
+words = words.filter((word) => word.length == 5);
 
-router.get("/wordle/solver", (req, res) => {
-  res.render("wordle/solver");
-});
+// router.get("/wordle/solver", (req, res) => {
+//   res.render("wordle/solver");
+// });
 
-router.get("/wordfinder/:params", (req, res) => {
-  console.log(req.query);
-  console.log("on wordfinder");
-  res.json({ a: "test" });
-});
+// router.post("/wordfinder/", (req, res) => {
+//   let { letter1, letter2, letter3, letter4, letter5 } = req.body;
+//   console.log(letter1, letter2, letter3, letter4, letter5);
+//   res.json({ guess: "test" });
+// });
 
-module.exports = router;
+// module.exports = router;
+
+// [.n..e] 'a' somewhere, no 'bf', ---- ankle
